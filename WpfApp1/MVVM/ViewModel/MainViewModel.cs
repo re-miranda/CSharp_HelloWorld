5b0dbf8558e0359143d0c0bd2f1a7408048635c9
re-miranda <pesca_botoes.0t@icloud.com>
2023-08-28 15:51:06 -0700

    Added both projects

---
+++ WpfApp1/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using WpfApp1.Core;
+
+namespace WpfApp1.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand OtherViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+        public OtherViewModel OtherVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            OtherVM = new OtherViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+            OtherViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = OtherVM;
+            });
+        }
+    }
+}
